68bf8b08564a15e7e51c9d7d62ac2f44962c9d63
Mehdi Ayache <ayache@kizeo.com>
2019-06-12 15:18:50 +0200

    update

--- FireAndForget.php
+++ FireAndForget.php
@@ -13,12 +13,12 @@ final class FireAndForget
 
     public function get($url, $params)
     {
-        $this->fire("GET", $url, $params);
+        $this->fire(strtoupper(__FUNCTION__), $url, $params);
     }
 
     public function post($url, $params)
     {
-        $this->fire("POST", $url, $params);
+        $this->fire(strtoupper(__FUNCTION__), $url, $params);
     }
 
     private function getDefaultPort($scheme)
